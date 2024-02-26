288462842009a0e42f42fdbea2b6c431590006ba
Nikolay Kravchenko <nikolay.kravchenko@alkima.org>
2024-02-26 20:08:35 +0200

    slider - get from db

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,23 +22,6 @@
 
 ])
     <x-main-slider/>
-    <x-services-mini :services="$services"/>
-    <x-about-us
-        title="CarServ Is The Best Place For Your Auto Care"
-        text="<p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos.</p><p>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>"
-        link="/abc"
-        :benefits="[
-            ['num' => '01', 'title' => 'Professional & Expert', 'text' => 'Diam dolor diam ipsum sit amet diam et eos'],
-            ['num' => '02', 'title' => 'Quality Servicing Center', 'text' => 'Diam dolor diam ipsum sit amet diam et eos'],
-            ['num' => '03', 'title' => 'Awards Winning Workers', 'text' => 'Diam dolor diam ipsum sit amet diam et eos'],
-    ]"/>
-    <x-fact
-        :facts="[
-            ['faClass' => 'fa-tools', 'count' => '1234', 'text' => 'Years Experience'],
-            ['faClass' => 'fa-users-cog', 'count' => '4321', 'text' => 'Expert Technicians'],
-            ['faClass' => 'fa-users', 'count' => '4433', 'text' => 'Satisfied Clients'],
-            ['faClass' => 'fa-car', 'count' => '1122', 'text' => 'Compleate Projects'],
-        ]"/>
     <x-services
         :services="[
         [
@@ -74,10 +57,23 @@
             'link' => '/aaa'
         ],
     ]"/>
-    <x-booking
-        title="Certified and Award Winning Car Repair Service Provider"
-        text="Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit. Sea dolore sanctus sed et. Takimata takimata sanctus sed."
-    />
+{{--    <x-services-mini :services="$services"/>--}}
+{{--    <x-about-us--}}
+{{--        title="CarServ Is The Best Place For Your Auto Care"--}}
+{{--        text="<p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos.</p><p>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>"--}}
+{{--        link="/abc"--}}
+{{--        :benefits="[--}}
+{{--            ['num' => '01', 'title' => 'Professional & Expert', 'text' => 'Diam dolor diam ipsum sit amet diam et eos'],--}}
+{{--            ['num' => '02', 'title' => 'Quality Servicing Center', 'text' => 'Diam dolor diam ipsum sit amet diam et eos'],--}}
+{{--            ['num' => '03', 'title' => 'Awards Winning Workers', 'text' => 'Diam dolor diam ipsum sit amet diam et eos'],--}}
+{{--    ]"/>--}}
+    <x-fact
+        :facts="[
+            ['faClass' => 'fa-tools', 'count' => '1234', 'text' => 'Years Experience'],
+            ['faClass' => 'fa-users-cog', 'count' => '4321', 'text' => 'Expert Technicians'],
+            ['faClass' => 'fa-users', 'count' => '4433', 'text' => 'Satisfied Clients'],
+            ['faClass' => 'fa-car', 'count' => '1122', 'text' => 'Compleate Projects'],
+        ]"/>
     <x-team
         :employees="[
     ['name' => 'Lorem Ipsum', 'position' => 'Designer', 'avatar' => 'img/team-1.jpg', 'social_fb' => 'https://www.facebook.com/', 'social_inst' => 'https://www.instagram.com/', 'social_tw' => 'https://twitter.com/'],
@@ -85,6 +81,10 @@
     ['name' => 'Ipsum Aliquyam', 'position' => 'Manager', 'avatar' => 'img/team-3.jpg'],
     ['name' => 'Ipsum Ipsum', 'position' => 'Copywriter', 'avatar' => 'img/team-4.jpg'],
 ]"
+    />
+    <x-booking
+        title="Certified and Award Winning Car Repair Service Provider"
+        text="Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit. Sea dolore sanctus sed et. Takimata takimata sanctus sed."
     />
 
     <x-reviews
